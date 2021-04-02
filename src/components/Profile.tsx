0c53dd5f01fcf71af8726fff3c4108a8462980da
Henrique Luís Oliveira Marques <henriquestudo@outlook.com>
2021-04-02 07:18:34 -0300

    Bug concerted

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -24,7 +24,7 @@ export function Profile() {
     <ProfileContainer>
       <img src={session.user.image} alt={session.user.name} />
       <div>
-        <strong>{session.user.id}</strong>
+        <strong>{session.user.name}</strong>
         <p>
           <img src="icons/level.svg" alt="Seu level" />
           Level {level}
